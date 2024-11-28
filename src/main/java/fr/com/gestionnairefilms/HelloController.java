3986c8be7e45c50f14a996af33c8755b37342158
Mathis <mathisdumage0@gmail.com>
2024-11-28 16:53:00 +0100

    CSS + actions en cliquant sur les cellules du tableau

--- src/main/java/fr/com/gestionnairefilms/HelloController.java
+++ src/main/java/fr/com/gestionnairefilms/HelloController.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseButton;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -22,6 +23,20 @@ public class HelloController {
         Film nouveauFilm = new Film("Inception", 9, 2010, true, null, "Christopher Nolan");
         tableView.getItems().add(nouveauFilm);
         loadFilms();
+
+        // Ajouter un écouteur d'événements au TableView
+        tableView.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2 && event.getButton() == MouseButton.PRIMARY) {
+                Film selectedFilm = tableView.getSelectionModel().getSelectedItem();
+                if (selectedFilm != null) {
+                    afficherDetailsFilm(selectedFilm);
+                }
+            }
+        });
+    }
+
+    public void afficherDetailsFilm(Film selectedFilm) {
+        System.out.println(selectedFilm);
     }
 
     private void loadFilms() {
@@ -35,25 +50,18 @@ public class HelloController {
                 JSONObject filmJson = (JSONObject) obj;
                 String titre = (String) filmJson.get("titre");
 
-                // It is necessary to import it as a long and then convert it to int
-                // Caused by the org.simple.json
+                // It is necessary to import it as a long and then convert it to int (Caused by the org.simple.json)
                 Long noteLong = (Long) filmJson.get("note");
                 int note = noteLong.intValue();
-
                 Long dateLong = (Long) filmJson.get("year");
                 int dateSortie = dateLong.intValue();
 
                 boolean visionneParUtilisateur = (boolean) filmJson.get("visionneParUtilisateur");
-
                 String realisateur = (String) filmJson.get("director");
-
                 List<String> acteurs = (List<String>) filmJson.get("actors");
 
-
-                System.out.println(acteurs);
-                films.add(new Film(titre, note , dateSortie ,visionneParUtilisateur ,acteurs ,realisateur));
+                films.add(new Film(titre, note, dateSortie, visionneParUtilisateur, acteurs, realisateur));
             }
-
         }
         tableView.setItems(films);
     }
